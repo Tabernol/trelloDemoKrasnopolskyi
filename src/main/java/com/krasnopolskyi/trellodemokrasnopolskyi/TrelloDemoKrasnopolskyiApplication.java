6cbf532a4bbc4fae06bd46bb73872ab0fc5d64ac
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2023-11-08 00:52:45 +0200

    Initial commit

---
+++ src/main/java/com/krasnopolskyi/trellodemokrasnopolskyi/TrelloDemoKrasnopolskyiApplication.java
@@ -0,0 +1,13 @@
+package com.krasnopolskyi.trellodemokrasnopolskyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloDemoKrasnopolskyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloDemoKrasnopolskyiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasnopolskyi/trellodemokrasnopolskyi/TrelloDemoKrasnopolskyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnopolskyi.trellodemokrasnopolskyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloDemoKrasnopolskyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
